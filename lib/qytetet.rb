25e9a11fdf1aecedd4721f5b269d78ee4920869e
Antonio David Villegas Yeguas <advy99@gmail.com>
2018-10-19 16:03:08 +0200

    eliminados ; innecesarios

--- lib/qytetet.rb
+++ lib/qytetet.rb
@@ -41,39 +41,39 @@ module ModeloQytetet
         def inicializar_cartas_sorpresa
             
             @mazo << Sorpresa.new("Un fan anónimo ha pagado tu fianza. Sales " +
-                 "de la cárcel", 0, TipoSorpresa::SALIRCARCEL);
+                 "de la cárcel", 0, TipoSorpresa::SALIRCARCEL)
         
             @mazo << Sorpresa.new("Por ir tan rápido debes pagar una multa de" +
-                    "500 euros", -500, TipoSorpresa::PAGARCOBRAR);
+                    "500 euros", -500, TipoSorpresa::PAGARCOBRAR)
 
             @mazo << Sorpresa.new("Por ser tan buen programador te damos un " +
-                    "premio de 1000 euros", 1000, TipoSorpresa::PAGARCOBRAR);
+                    "premio de 1000 euros", 1000, TipoSorpresa::PAGARCOBRAR)
 
             @mazo << Sorpresa.new("Es tu cumpleaños y tus compañeros son" +
                    " generosos, cada jugador te da 200 euros", 200,
-                    TipoSorpresa::PORJUGADOR);
+                    TipoSorpresa::PORJUGADOR)
 
             @mazo << Sorpresa.new("No era tu cumpleaños, ¡mentiroso!. Debes " +
                    "darle a los demás lo que te han dado con intereses, " + 
-                   "300 euros a cada uno", -300, TipoSorpresa::PORJUGADOR);
+                   "300 euros a cada uno", -300, TipoSorpresa::PORJUGADOR)
              
             @mazo << Sorpresa.new("Eres muy emprendedor, obtienes 100 euros "+
                    "por cada casa y hotel que poseas",
-                    100, TipoSorpresa::PORCASAHOTEL);
+                    100, TipoSorpresa::PORCASAHOTEL)
 
             @mazo << Sorpresa.new("Tienes demasiadas propiedades, deja algo " +
                    "para los demás. Paga 150 euros por cada casa y hotel", -150, 
-                    TipoSorpresa::PORCASAHOTEL);
+                    TipoSorpresa::PORCASAHOTEL)
 
             @mazo << Sorpresa.new("Te hemos pillado con chanclas y calcetines," +
                     "lo sentimos, ¡debes ir a la carcel!",
-                     @tablero.carcel.numero_casilla, TipoSorpresa::IRACASILLA);
+                     @tablero.carcel.numero_casilla, TipoSorpresa::IRACASILLA)
 
             @mazo << Sorpresa.new("Has tenido suerte, vas a cobrar sin trabajar." +
-                    "Ve a la casilla de salida.", 0, TipoSorpresa::IRACASILLA);
+                    "Ve a la casilla de salida.", 0, TipoSorpresa::IRACASILLA)
 
             @mazo << Sorpresa.new("Encuentras un atajo en el camino.",
-                    18, TipoSorpresa::IRACASILLA);
+                    18, TipoSorpresa::IRACASILLA)
         end
         
                 
